6af0e7e5a6d722cd1afc3f90c8df8c87972fa9e5
KeslieBox <kesliebox@gmail.com>
2021-01-14 21:55:44 -0600

    commit tree 81cb753bbdba6093b47a9ca0d877aad036f3680b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    if array.size >= 3
+       last_word = array.pop
+       array.push('and ' + last_word)
+       array.join(', ')
+    elsif array.size == 2
+        array.join(' and ')
+    else 
+        array.join
+    end 
 end
